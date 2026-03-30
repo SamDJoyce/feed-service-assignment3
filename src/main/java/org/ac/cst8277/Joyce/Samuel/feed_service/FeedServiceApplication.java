63c0897a9626c81453ea9071f8d1856a6502c425
Sam Joyce <samdjoyce@gmail.com>
2026-03-30 11:59:05 -0400

    initial commit

---
+++ src/main/java/org/ac/cst8277/Joyce/Samuel/feed_service/FeedServiceApplication.java
@@ -0,0 +1,13 @@
+package org.ac.cst8277.Joyce.Samuel.feed_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FeedServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FeedServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/ac/cst8277/Joyce/Samuel/feed_service/FeedService/FeedServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.ac.cst8277.Joyce.Samuel.feed_service.FeedService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FeedServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
